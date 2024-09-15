6344b1c09c19e38bbf2f82c70c322585bc719c19
rlylo <lylomain@gmail.com>
2024-09-15 14:37:57 -0500

    give me 5 dollars created

---
+++ src/week07/GiveMe5Dollars.java
@@ -0,0 +1,19 @@
+package week07;
+
+import java.util.Scanner;
+
+public class GiveMe5Dollars {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int dollars;
+        do {
+            System.out.println("Please enter 5 dollars");
+            dollars = input.nextInt();
+        } while (dollars != 5);
+        input.close();
+
+        System.out.println("Thank you for $5");
+
+
+    }
+}
