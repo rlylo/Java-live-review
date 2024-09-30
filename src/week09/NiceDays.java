e91872859edba4f418019bc212064d378a6015b5
rlylo <lylomain@gmail.com>
2024-09-30 13:20:42 -0500

    File NiceDays created , code tested

---
+++ src/week09/NiceDays.java
@@ -0,0 +1,17 @@
+package week09;
+
+public class NiceDays {
+    public static void main(String[] args) {
+        int[] temps = {66, 75, 78, 92, 72, 80, 90};
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        //Nice weather days ->(70-80)
+        for (int i = 0; i < temps.length; i++) {
+            if (temps[i] >= 70 && temps[i] <= 80) {
+                System.out.println("Nice weather day " + temps[i] + " on " + days[i]);
+            }
+        }
+
+
+    }
+}
