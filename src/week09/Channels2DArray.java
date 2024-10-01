71106df887775bc09c7780b4d5a001521a77d7db
rlylo <lylomain@gmail.com>
2024-10-01 09:42:21 -0500

    Multidimention array example added

---
+++ src/week09/Channels2DArray.java
@@ -0,0 +1,13 @@
+package week09;
+
+import java.util.Arrays;
+
+public class Channels2DArray {
+    public static void main(String[] args) {
+        String[][] messages = {{"Hi", "What is and array"},
+                {"Code Solution", " Arrays task code"},
+                {"Group study", "When is next class"}};
+
+        System.out.println(Arrays.deepToString(messages));
+    }
+}
