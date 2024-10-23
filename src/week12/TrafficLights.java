f11331d471ca4aa2173850e0f01b0ba11330935f
rlylo <lylomain@gmail.com>
2024-10-23 12:07:56 -0500

    adding file TrafficLights.java

---
+++ src/week12/TrafficLights.java
@@ -0,0 +1,14 @@
+package week12;
+
+public class TrafficLights {
+
+    private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
