aeda7b74f139684be67c6b02e3d6e9b20e13f201
rlylo <lylomain@gmail.com>
2024-11-03 11:00:47 -0600

    flag package added

---
+++ src/week14/flag/FlagClients.java
@@ -0,0 +1,10 @@
+package week14.flag;
+
+public class FlagClients {
+    public static void main(String[] args) {
+
+        FranceFlag flag = new FranceFlag();
+        flag.drawFlag();
+
+    }
+}
